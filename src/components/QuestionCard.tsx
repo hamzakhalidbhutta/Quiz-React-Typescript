5caf0081a1ecd8457442daaf67e484b15c64dccb
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-08 06:19:49 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,46 +2,56 @@ import React, { useEffect } from 'react'
 import moment from 'moment';
 import { Card, Grid, CardContent, Button, Typography } from '@material-ui/core';
 
-const QuestionCard = (props: any) => {
+interface IProps {
+    startQuiz: any;
+    calculateTimer: any;
+    timer: any;
+    question: any;
+    questionNumber: any;
+    currentQuestion: any;
+    storeAnswers: any;
+    ready: any;
+}
+
+const QuestionCard = ({ startQuiz, calculateTimer, timer, question, questionNumber, currentQuestion, storeAnswers, ready, }: IProps) => {
 
     useEffect(() => {
-        props.startQuiz(true)
-    
-        props.calculateTimer()
-    }, [])
-    // useEffect(() => {
-    // }, [props.calculateTimer()])
 
-    
+            startQuiz(true)
+            calculateTimer()
+        
+    },[])
+
+ 
     return (
         <>
             {
-                (props.ready ) ?
+                (ready) ?
                     (
                         <Card variant="outlined" className="card">
 
                             <CardContent>
-                                <Typography color="textSecondary" gutterBottom>{moment.utc(props.timer*1000).format('HH:mm:ss')}</Typography>
+                                <Typography color="textSecondary" gutterBottom>{moment.utc(timer * 1000).format('HH:mm:ss')}</Typography>
                                 {
-                                    props.question.map((v: any, i: number) =>
-                                        (i === props.questionNumber && props.timer > 0) ?
+                                    question.map((v: any, i: number) =>
+                                        (i === questionNumber && timer > 0) ?
                                             (
                                                 <div key={i}>
 
                                                     <Typography color="textSecondary" gutterBottom>
-                                                        {props.questionNumber + 1}. {v.quiz_question}
+                                                        {questionNumber + 1}. {v.quiz_question}
                                                     </Typography>
                                                     {
                                                         v.answers.map((v: any, j: number) =>
                                                         (<div key={j}>
-                                                            <Button variant="contained" fullWidth color="primary" value={v[0]} onClick={(e) => { e.preventDefault(); props.storeAnswers(v[0]) }} >  {v[0]}</Button>
+                                                            <Button variant="contained" fullWidth color="primary" value={v[0]} onClick={(e) => { e.preventDefault(); storeAnswers(v[0]) }} >  {v[0]}</Button>
                                                             <br />  <br />
-                                                            <Button variant="contained" fullWidth color="primary" value={v[1]} onClick={(e) => { e.preventDefault(); props.storeAnswers(v[1]) }} >  {v[1]}</Button>
+                                                            <Button variant="contained" fullWidth color="primary" value={v[1]} onClick={(e) => { e.preventDefault(); storeAnswers(v[1]) }} >  {v[1]}</Button>
                                                             <br />
                                                             <br />
-                                                            <Button variant="contained" fullWidth color="primary" value={v[2]} onClick={(e) => { e.preventDefault(); props.storeAnswers(v[2]) }} >  {v[2]}</Button>
+                                                            <Button variant="contained" fullWidth color="primary" value={v[2]} onClick={(e) => { e.preventDefault(); storeAnswers(v[2]) }} >  {v[2]}</Button>
                                                             <br />  <br />
-                                                            <Button variant="contained" fullWidth color="primary" value={v[3]} onClick={(e) => { e.preventDefault(); props.storeAnswers(v[3]) }} >  {v[3]}</Button>
+                                                            <Button variant="contained" fullWidth color="primary" value={v[3]} onClick={(e) => { e.preventDefault(); storeAnswers(v[3]) }} >  {v[3]}</Button>
                                                             <br />  <br />
                                                         </div>
                                                         ))
@@ -49,10 +59,10 @@ const QuestionCard = (props: any) => {
                                                     <br />
                                                     <Grid container spacing={3}>
                                                         <Grid item xs={8}>
-                                                            <Button onClick={(e) => { e.preventDefault(); props.currentQuestion(false); }}>Previous</Button>
+                                                            <Button onClick={(e) => { e.preventDefault(); currentQuestion(false); }}>Previous</Button>
                                                         </Grid>
                                                         <Grid item xs={4}>
-                                                            <Button onClick={(e) => { e.preventDefault(); props.currentQuestion(true); }}>Next</Button>
+                                                            <Button onClick={(e) => { e.preventDefault(); currentQuestion(true); }}>Next</Button>
                                                         </Grid>
                                                     </Grid>
                                                 </div>
@@ -60,7 +70,7 @@ const QuestionCard = (props: any) => {
                                             ) : null
                                     )
                                 }
-                                <Button variant="contained" fullWidth onClick={(e) => { e.preventDefault(); props.startQuiz(false); }}>Finish</Button>
+                                <Button variant="contained" fullWidth onClick={(e) => { e.preventDefault(); startQuiz(false); }}>Finish</Button>
                             </CardContent>
                         </Card>) :
                     null
